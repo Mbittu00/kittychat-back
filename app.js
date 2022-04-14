a7c3621f19ec4271db2903644c735aafef323fba
bittu <mbittu000@gmail.com>
2022-04-15 00:40:25 +0530

    main thin is done now

---
+++ app.js
@@ -0,0 +1,50 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io=require('socket.io')(http,
+{cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }})
+app.get('/', function(req, res) {
+   res.send('hello')
+});
+let user=[]
+let msg=[]
+
+//function
+
+
+
+
+io.on('connection',(socket)=>{
+
+//join
+socket.on('join',(res)=>{
+  let data={username:res,text:`${res} jast join`}
+ msg.push(data) 
+ io.emit('show',msg)
+ // console.log(user)
+})
+
+//show msg
+io.emit('show',msg)
+
+//send msg
+socket.on('msg',(res)=>{
+msg.push(res)
+//console.log(msg)
+io.emit('show',msg)
+})
+ 
+ 
+ 
+ socket.on('disconnect', ()=>{
+   console.log('disconnect')
+   });
+})
+
+
+
+http.listen(process.env.PORT||3030, function() {
+   console.log('listening on *:3030');
+});
