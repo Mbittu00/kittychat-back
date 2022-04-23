bfa909059d97a31aaa3b5d180352fc48fb175a97
bittu <mbittu000@gmail.com>
2022-04-23 19:57:11 +0530

    file send

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.use(bodyParser.urlencoded({limit:'5mb'}))
 
 io.on('connection',(socket)=>{
 
+io.emit('online',true)
+
 //join
 socket.on('join',(res)=>{
   let data={username:res,text:`${res} jast join`}
